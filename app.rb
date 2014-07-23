46f123d45d785aaa388d9e62ffa85f63aaa02b3a
Brian Yee <brian.yee@eprize.com>
2014-07-23 16:06:35 -0400

    apostrophe fixes

--- app.rb
+++ app.rb
@@ -63,7 +63,7 @@ get_or_post '/sms/?' do
     # send note to BH
     notify_bh(the_from_num, the_text);
     response = Twilio::TwiML::Response.new do |r|
-      r.Sms "As u wait for a response, consider watching the next Prophet’s Stories video at http://prophetstories.org/michigan/en_06/, Musa and the Blood of Sacrifice"
+      r.Sms "As u wait for a response, consider watching the next Prophet's Stories video at http://prophetstories.org/michigan/en_06/, Musa and the Blood of Sacrifice"
     end
     response.text
   when /blood/i
@@ -103,7 +103,7 @@ get_or_post '/sms/?' do
     response.text
   when /straight path|straightpath/i
     response = Twilio::TwiML::Response.new do |r|
-      r.Sms "Thank you for watching the Prophet’s Stories.  Would you be interested in a study of the signs of the prophets in the Holy Books? Please reply YES or NO"
+      r.Sms "Thank you for watching the Prophet's Stories. Would you be interested in a study of the signs of the prophets in the Holy Books? Please reply YES or NO"
     end
     response.text
   when /yes/i
